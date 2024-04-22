edf4c0fab7106595b9d7cdff32333c2d98693320
mingzq <north_sea@qq.com>
2024-04-22 06:12:59 +0000

    fixed: build.sh: auto gen sd img

--- external/build.sh
+++ external/build.sh
@@ -9,7 +9,7 @@ source $EXTERNAL/setenv.sh
 defconfig $1
 build_all
 
-if [ -n $SUDO_PWD ]; then
+if [ -n "$SUDO_PWD" ]; then
 pushd $PROJECT_OUT
 echo $SUDO_PWD | sudo -S ./build/tools/common/sd_tools/sd_gen_burn_image.sh $(realpath install/soc_$1) $1
 popd
